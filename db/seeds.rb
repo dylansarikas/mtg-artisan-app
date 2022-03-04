72c129dccc7c4e958b7ef26723b1175e3ec75099
dylan <dylan.sarikas@gmail.com>
2022-03-03 20:25:20 -0600

    adds seed data (associations work (i think (i hope)))

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#Deck.create(name: "", user_id: )
+Deck.create(name: "Mardu Reanimator", user_id: 1)
+Deck.create(name: "Mystical Teachings", user_id: 2)
+Deck.create(name: "Walls Combo", user_id: 3)
+
+#CardDeck.create(deck_id: , multiverse_id: )
+CardDeck.create(deck_id: 1, multiverse_id: 227087)
+CardDeck.create(deck_id: 2, multiverse_id: 111058)
+CardDeck.create(deck_id: 3, multiverse_id: 253556)

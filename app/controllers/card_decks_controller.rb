9ee61be146397847224d0a2abcdbba587693ec5c
dylan <dylan.sarikas@gmail.com>
2022-03-11 11:54:56 -0600

    adds logic in carddecks controller to limit deck size to be 100

--- app/controllers/card_decks_controller.rb
+++ app/controllers/card_decks_controller.rb
@@ -1,28 +1,36 @@
 class CardDecksController < ApplicationController
 
   def create
-    carddeck = CardDeck.new(
-      deck_id: params[:deck_id],
-      multiverse_id: params[:multiverse_id],
-      amount: params[:amount]
-    )
-    if carddeck.save
-      render json:carddeck
-    else render json: { errors: carddeck.errors.full_messages }, status: :bad_request
-    end
-  end
-
-  def update
-    carddeck = CardDeck.find(params[:id])
-    if params[:amount] == 0
-      carddeck.delete
-      render json:{message: "The CardDeck #{carddeck.id} was destroyed.  Do you feel good about yourself?"}
-    else
-      carddeck.amount = params[:amount] || carddeck.amount
+    if (params[:amount] + Deck.find(params[:deck_id]).deck_size) < 101
+      carddeck = CardDeck.new(
+        deck_id: params[:deck_id],
+        multiverse_id: params[:multiverse_id],
+        amount: params[:amount]
+      )
       if carddeck.save
         render json:carddeck
       else render json: { errors: carddeck.errors.full_messages }, status: :bad_request
       end
+    else
+      render json: {error: "Deck Cannot Exceed 100 total cards"}
+    end
+  end
+
+  def update
+    if (params[:amount] + CardDeck.find(params[:id]).deck.deck_size - CardDeck.find(params[:id]).amount) < 101
+      carddeck = CardDeck.find(params[:id])
+      if params[:amount] == 0
+        carddeck.delete
+        render json:{message: "The CardDeck #{carddeck.id} was destroyed.  Do you feel good about yourself?"}
+      else
+        carddeck.amount = params[:amount] || carddeck.amount
+        if carddeck.save
+          render json:carddeck
+        else render json: { errors: carddeck.errors.full_messages }, status: :bad_request
+        end
+      end
+    else
+      render json: {error: "Deck Cannot Exceed 100 total cards"}
     end
   end
 

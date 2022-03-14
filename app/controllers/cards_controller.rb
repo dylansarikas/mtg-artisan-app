02b29782fcc5f05f3ffb514d8eb231084406be12
dylan <dylan.sarikas@gmail.com>
2022-03-14 12:43:15 -0500

    does temporary change to card index

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,8 @@
 class CardsController < ApplicationController
 
   def index
-    response = HTTP.get("https://api.scryfall.com/cards/search?order=cmc&q=c%3A#{params[:color]}+pow%3D#{params[:power]}+f:#{params[:format]}+#{params[:name]}+r%3C=uncommon+t:#{params[:type]}")
+    # response = HTTP.get("https://api.scryfall.com/cards/search?order=cmc&q=c%3A#{params[:color]}+pow%3D#{params[:power]}+f:#{params[:format]}+#{params[:name]}+r%3C=uncommon+t:#{params[:type]}")
+    response = HTTP.get("https://api.scryfall.com/cards/named?fuzzy=unburialrites")
     
     render json: response.parse(:json)
   end

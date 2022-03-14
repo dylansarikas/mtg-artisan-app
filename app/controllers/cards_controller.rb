085a397b96c4d861e16873b07234ff5cf1e00b4f
dylan <dylan.sarikas@gmail.com>
2022-03-14 17:17:10 -0500

    starts associated data deletion

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,8 @@ class CardsController < ApplicationController
 
   def index
     # response = HTTP.get("https://api.scryfall.com/cards/search?order=cmc&q=c%3A#{params[:color]}+pow%3D#{params[:power]}+f:#{params[:format]}+#{params[:name]}+r%3C=uncommon+t:#{params[:type]}")
-    response = HTTP.get("https://api.scryfall.com/cards/named?fuzzy=unburialrites")
+    # response = HTTP.get("https://api.scryfall.com/cards/named?fuzzy=unburialrites")
+    response = HTTP.get("https://api.scryfall.com/cards/named?fuzzy=#{params[:search]}")
     
     render json: response.parse(:json)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :decks
+  # has_many :decks, depedent: :destroy
 
 end
